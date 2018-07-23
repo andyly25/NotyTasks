2c7b8521b26f37a1e4e2dfed10dfaf420ce19130
Andy Ly <andywindsong@gmail.com>
2018-07-23 14:23:08 -0700

    fixed some eslint errors

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -5,37 +5,36 @@ const Task = require('../models/task.model');
 exports.getTasks = (req, res) => {
   Task
     .find()
-    .then(tasks => {
-      res.json(tasks.map(task => task))
-    })
-}
+    .then((tasks) => {
+      res.json(tasks.map(task => task));
+    });
+};
 
 // Using POST
 exports.postTask = (req, res) => {
   // console.log(req.body);
   Task
     .create(req.body)
-    .then(task => {
+    .then((task) => {
       // console.log(task);
       res.json(task);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-    })
-}
-
+    });
+};
 
 // Using DELETE
 exports.deleteTask = (req, res) => {
   Task
     .findByIdAndRemove(req.params.id)
     // ShoppingList.delete(req.params.id);
-    .then (task => {
+    .then ((task) => {
       console.log(`Deleted task item \`${req.params.id}\``);
       res.status(204).end();
-      res.json({id: task._id});
-    })
-}
+      res.json({ id: task._id });
+    });
+};
 
 // Using PUT
 // exports.putTask
